821d58b75edc5bcacde88629be32a2bf9791d87c
Evros <evross94@gmail.com>
2019-10-02 01:49:34 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Select DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItem = document.querySelectorAll(".nav-item");
+
+//Set Initial State of Menu
+let showMenu = false; //we use let because we want to directly reassing it in certain times. You can't do that with const
+
+//Event Listener
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItem.forEach(item => item.classList.add("show")); //They are more than one, so we need to loop through them. For each nav item add the show class
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItem.forEach(item => item.classList.remove("show")); //They are more than one, so we need to loop through them. For each nav item add the show class
+
+    // Set Menu State
+    showMenu = false;
+  }
+}
